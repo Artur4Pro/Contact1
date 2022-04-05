3317e2af9d821efb3e30376d8cc2185569dc993f
ZohrabJS <zoro.77zz777@mail.ru>
2022-04-05 10:18:44 +0400

    3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import view.Start;
 
 public class Main {
     public static void main(String[] args) {
-
         Start start = new Start();
         start.start();
         System.out.println("dsjlnckj");
